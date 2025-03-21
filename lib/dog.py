dbe2c5a7759a36cf83e6e0a8e1619b72968b8769
Perpetual Akinyi <you@example.com>
2025-03-21 13:17:40 +0300

    working on getting it right

--- lib/dog.py
+++ lib/dog.py
@@ -12,4 +12,33 @@ APPROVED_BREEDS = [
 ]
 
 class Dog:
-    pass
+    def __init__(self, name=None, breed=None):
+        if name is not None:
+            self.name = name  # Calls the setter
+        else:
+            self._name = None  # Allow initialization without name
+        
+        if breed is not None:
+            self.breed = breed  # Calls the setter
+        else:
+            self._breed = None  # Allow initialization without breed
+
+    @property
+    def name(self):
+        return self._name
+
+    @name.setter
+    def name(self, value):
+        if not isinstance(value, str) or  (1 <= len(value) <= 25):
+            print("Name must be string between 1 and 25 characters.")
+        self._name = value
+
+    @property
+    def breed(self):
+        return self._breed
+
+    @breed.setter
+    def breed(self, value):
+        if value not in APPROVED_BREEDS:
+            print("Breed must be in list of approved breeds.")
+        self._breed = value
--- lib/person.py
+++ lib/person.py
@@ -16,4 +16,27 @@ APPROVED_JOBS = [
 ]
 
 class Person:
-    pass
+    def __init__(self, name, job):
+        self.name = name
+        self.job = job
+
+    @property
+    def name(self):
+        return self._name
+    
+    @name.setter
+    def name(self, value):
+        if not isinstance(value, str) or not (1 <= len(value) <= 25):
+            raise TypeError("Name must be a string between 1 and 25 characters.")
+        self._name = value
+
+    @property
+    def job(self):
+        return self._job
+    
+    @job.setter
+    def job(self, value):
+        if not isinstance(value, str) or not (1 <= len(value) <= 25):
+            raise TypeError("Job must be a string between 1 and 25 characters.")
+        if value not in APPROVED_JOBS:
+            raise ValueError("Job must be in list of approved jobs")
